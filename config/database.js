e7b5be17f3c23bc05bdea9922fdcab5fbe5b67d8
hannah-devine <hannah96@hotmail.be>
2021-03-29 10:41:17 +0200

    www aangepast

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'ID309897_blogs'),
         username: env('DATABASE_USERNAME', 'ID309897_blogs'),
-        password: env('DATABASE_PASSWORD', 'ikhaatblogs'),
+        password: env('DATABASE_PASSWORD', 'ikhaatblogs15'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
