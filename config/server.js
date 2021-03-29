be77440b0c370bca93ee7a9babbe0c10ba667e0c
hannah-devine <hannah96@hotmail.be>
2021-03-29 09:50:44 +0200

    seconde commmit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1a1bd9e20af49bd892571bb1ce6ff5da'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '93cf239f-7529-47d1-aa64-84db4b0683c5'
+};
